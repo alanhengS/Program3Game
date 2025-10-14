0213b57f25171d02853ca97c5bcc3c24e3b0e647
AlanaDubie <alanadubie922@gmail.com>
2025-10-13 22:29:41 -0700

    formatted Coin.cs and CoinManager.cs scripts to proper documentation

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -1,5 +1,19 @@
+/***************************************************************
+* file: Coin.cs
+* author: Alana Dubie
+* class: CS 4700 – Game Development
+* assignment: Program 3
+* date last modified: 10/13/2025
+*
+* purpose: This script handles coin collection logic in the game.
+* When the player collides with a coin, the coin value is added
+* to the CoinManager, and the coin object is destroyed.
+*
+****************************************************************/
 
 using UnityEngine;
+
+// Detects player collision with a coin object and updates the coin count
 public class Coin : MonoBehaviour
 {
     [SerializeField] public int coinValue;
@@ -8,16 +22,18 @@ public class Coin : MonoBehaviour
 
     private CoinManager coinManager;
 
+    // Initializes the coinManager reference
     private void Start()
     {
         coinManager = CoinManager.instance;
     }
 
+    // Detects collision with the player, updates coin count, and destroys the coin
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && !hasTriggered )
+        if (collision.CompareTag("Player") && !hasTriggered)
         {
-            hasTriggered = true; 
+            hasTriggered = true;
             coinManager.ChangeCoins(coinValue);
             Destroy(gameObject);
         }
